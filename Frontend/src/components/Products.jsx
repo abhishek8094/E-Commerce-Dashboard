396a6460e9d93d6898466c55a9d6ab3285c67f5b
abhishek8094 <iasabhi41@gmail.com>
2024-10-30 18:44:38 +0530

    new changes

--- Frontend/src/components/Products.jsx
+++ Frontend/src/components/Products.jsx
@@ -1,40 +1,54 @@
 import { useEffect, useState } from "react";
 
-const Products = ()=>{
-    const [products, setProducts] = useState([]);
+const Products = () => {
+  const [products, setProducts] = useState([]);
 
-    useEffect(() =>{
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    let result = await fetch("http://localhost:3000/products");
+    result = await result.json();
+    setProducts(result);
+
+    console.log(result);
+  };
+
+  const deleteProduct = async (id) => {
+    let result = await fetch(`http://localhost:3000/product/${id}`, {
+      method: "Delete",
+    });
+
+    result = await result.json();
+    if (result) {
         getProducts();
-    },[])
-
-    const getProducts = async() =>{
-        let result = await fetch("http://localhost:3000/products");
-        result = await result.json();
-        setProducts(result)
-
-        console.log(result);
     }
-    return(
-        <div className="products">
-        <h1>Products list</h1>
-        <ul>
-           <li>S. No.</li> 
-           <li>Name</li> 
-           <li>Price</li> 
-           <li>Category</li> 
+  };
+
+  return (
+    <div className="products">
+      <h1>Products list</h1>
+      <ul>
+        <li>S. No.</li>
+        <li>Name</li>
+        <li>Price</li>
+        <li>Category</li>
+        <li>Operation</li>
+      </ul>
+      {products.map((product, idx) => (
+        <ul key={idx}>
+          <li>{idx}</li>
+          <li>{product.name}</li>
+          <li>$ {product.price}</li>
+          <li>{product.Category}</li>
+          <li>
+            <button onClick={() => deleteProduct(product._id)}>Delete</button>
+          </li>
         </ul>
-        {
-            products.map((product, idx)=>(
-                <ul key={idx}>
-                <li>{idx}</li> 
-                <li>{product.name}</li> 
-                <li>$ {product.price}</li> 
-                <li>{product.Category}</li> 
-             </ul> 
-            ))
-        }
-        </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
 export default Products;
